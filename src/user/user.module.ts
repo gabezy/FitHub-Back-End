cc4fb457de0f571264ef2007129c40b540bede77
gabezy <gabrielmoreira2705@gmail.com>
2023-09-08 23:20:10 -0300

    chore: add Prisma User repository as injectable

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,13 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from 'src/infra/database/prisma.service';
 import { WorkoutModule } from 'src/workout/workout.module';
+import { UserRepository } from 'src/repositories/user-repository';
+import { PrismaUserRepository } from 'src/repositories/prisma/prisma-user-repository';
 
 @Module({
   imports: [WorkoutModule],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, { provide: UserRepository, useClass: PrismaUserRepository }],
   exports: [UserService, PrismaService],
 })
 export class UserModule { }
