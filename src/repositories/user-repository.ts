60306664c00221becb947d3f83486d5b73e263df
gabezy <gabrielmoreira2705@gmail.com>
2023-09-08 23:19:26 -0300

    feature: User Repository

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,6 +1,10 @@
 import { User } from '@prisma/client';
-import { CreateUserDto } from 'src/user/dtos/create-user.dto';
 
-abstract class UserRepository {
-  abstract create(data: CreateUserDto): Promise<User>;
+export abstract class UserRepository {
+  abstract create(data: { first_name: string; last_name: string; password: string; email: string }): Promise<User>;
+  abstract findUserById(userId: string): Promise<User>;
+  abstract findUserByEmail(email: string): Promise<User>;
+  abstract findAllUsers(page: number): Promise<User[]>
+  abstract updateUser(userId: string, data: { firstName?: string, lastName?: string }): Promise<void>;
+  abstract deleteUser(userId: string): Promise<void>
 }
