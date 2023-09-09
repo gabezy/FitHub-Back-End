b4c390c4c1ddefda564f933741e4173989d4310a
gabezy <gabrielmoreira2705@gmail.com>
2023-09-08 23:20:47 -0300

    chore: implement Prisma User Repository

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,63 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/infra/database/prisma.service';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { Prisma, User } from '@prisma/client';
+import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { UserRepository } from 'src/repositories/user-repository';
 
 @Injectable()
 export class UserService {
-  constructor(private prismaService: PrismaService) { }
+  constructor(private userRepository: UserRepository) { }
 
   async createUser(params: CreateUserDto): Promise<User> {
     const { password } = params;
     const hashedPassword = await bcrypt.hash(password, 6);
-    const newUser = await this.prismaService.user.create({
-      data: {
-        first_name: params.firstName,
-        last_name: params.lastName,
-        password: hashedPassword,
-        email: params.email,
-      },
+    const newUser = await this.userRepository.create({
+      first_name: params.firstName,
+      last_name: params.lastName,
+      password: hashedPassword,
+      email: params.email,
     });
     return newUser;
   }
 
-  async findUserById(id: string): Promise<User> {
-    const user = await this.prismaService.user.findUnique({
-      where: { id },
-    });
+  async findUserById(userId: string): Promise<User> {
+    const user = await this.userRepository.findUserById(userId);
     return user;
   }
 
   async findManyUsers(page?: number): Promise<User[]> {
     const skip = page !== undefined ? page - 1 : 0;
-    const users = await this.prismaService.user.findMany({
-      skip: skip,
-      take: 10,
-      orderBy: { id: 'asc' },
-    });
+    const users = await this.userRepository.findAllUsers(skip);
     return users;
   }
 
   async findUserByEmail(email: string): Promise<User> {
-    const user = await this.prismaService.user.findUnique({
-      where: { email: email },
-    });
+    const user = await this.userRepository.findUserByEmail(email);
     return user;
   }
 
-  async updateUser(params: {
-    id: string;
-    data: Prisma.UserUpdateInput;
-  }): Promise<void> {
-    const { id, data } = params;
-    await this.prismaService.user.update({
-      data: { ...data, updated_at: new Date() },
-      where: { id },
-    });
+  async updateUser(userId: string, data: { firstName: string, lastName: string }): Promise<void> {
+    await this.userRepository.updateUser(userId, { firstName: data.firstName, lastName: data.lastName })
   }
 
-  async deleteUSer(id: string): Promise<void> {
-    await this.prismaService.user.delete({ where: { id } });
+  async deleteUSer(userId: string): Promise<void> {
+    await this.userRepository.deleteUser(userId);
   }
 }
